096b68e42a5d857da7f38cf830ddc09967f5e6c4
diegacosta211 <diegacosta211@gmail.com>
2018-09-22 18:11:29 -0300

    Functional and Class Components

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ const Pet = props => {
   ]);
 };
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed"
-    })
-  ]);
-};
+class App extends React.Component {
+  handleTitleClick() {
+    alert("You clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed"
+      })
+    ]);
+  }
+}
 
 ReactDOM.render(
   // React.createElement can handle both strings tag
