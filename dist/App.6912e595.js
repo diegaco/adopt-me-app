70b95cc2b74dab91376709b605deb8ceb7a73b9b
diegaco <diegoacosta211@gmail.com>
2018-09-26 00:28:48 -0300

    remove Component constructor and implement class properties

--- dist/App.6912e595.js
+++ dist/App.6912e595.js
@@ -23956,6 +23956,12 @@ var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
+var _petfinderClient = require("petfinder-client");
+
+var _petfinderClient2 = _interopRequireDefault(_petfinderClient);
+
+var _router = require("@reach/router");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -23964,22 +23970,104 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var petfinder = (0, _petfinderClient2.default)({
+  key: "a824f5a589f6cb083de0c1fecb836ac0",
+  secret: "b15bb930def2c43ef59b820bc0fa6eb6"
+});
+
 var Details = function (_Component) {
   _inherits(Details, _Component);
 
   function Details() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
     _classCallCheck(this, Details);
 
-    return _possibleConstructorReturn(this, (Details.__proto__ || Object.getPrototypeOf(Details)).apply(this, arguments));
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Details.__proto__ || Object.getPrototypeOf(Details)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+      loading: true
+    }, _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(Details, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      petfinder.pet.get({
+        output: "full",
+        id: this.props.id
+      }).then(function (data) {
+        var pet = data.petfinder.pet;
+        var breed = void 0;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        _this2.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: pet.contact.city + ", " + pet.contact.state,
+          description: pet.description,
+          media: pet.media,
+          breed: breed,
+          loading: false
+        });
+      }).catch(function (err) {
+        (0, _router.navigate)("/");
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
+      if (this.state.loading) {
+        return _react2.default.createElement(
+          "h1",
+          null,
+          "Loading...."
+        );
+      }
+      var _state = this.state,
+          name = _state.name,
+          animal = _state.animal,
+          breed = _state.breed,
+          location = _state.location,
+          description = _state.description;
+
+
       return _react2.default.createElement(
-        "h1",
-        null,
-        "Lol"
+        "div",
+        { className: "details" },
+        _react2.default.createElement(
+          "div",
+          null,
+          _react2.default.createElement(
+            "h1",
+            null,
+            name
+          ),
+          _react2.default.createElement(
+            "h2",
+            null,
+            animal,
+            " - ",
+            breed,
+            " - ",
+            location
+          ),
+          _react2.default.createElement(
+            "p",
+            null,
+            description
+          )
+        )
       );
     }
   }]);
@@ -23988,7 +24076,7 @@ var Details = function (_Component) {
 }(_react.Component);
 
 exports.default = Details;
-},{"react":"../node_modules/react/index.js"}],"App.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","petfinder-client":"../node_modules/petfinder-client/index.js","@reach/router":"../node_modules/@reach/router/es/index.js"}],"App.js":[function(require,module,exports) {
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -24084,7 +24172,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52869' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53407' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
